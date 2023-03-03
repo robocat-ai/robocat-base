abf8592af750ecefb27a4cd7279bdd32e217f2b3
Aleksei Ivanov <contact@aleksei.dev>
2023-03-03 23:56:45 +0300

    Update bootstrap.sh

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -12,7 +12,7 @@ main() {
     local background_path="/usr/share/images/fluxbox/background.png"
     if [ -f "$background_path" ]; then
         feh --no-fehbg --bg-scale "$background_path"
-        log_i "Updated background image"
+        log_d "Updated background image"
     fi
 
     if [ "$VNC_ENABLED" = 1 ]; then
@@ -109,6 +109,12 @@ log_w() {
 
 log_e() {
     log "[ERROR] ${@}"
+}
+
+log_d() {
+    if [ -n "$DEBUG" ]; then
+        log "[DEBUG] ${@}"
+    fi
 }
 
 log() {
